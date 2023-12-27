3d7e65553f1bbf46d601d06b84108c0bfa1854b2
Slowlydev <slowly.developers@gmail.com>
2023-12-27 13:17:12 +0100

    feat: computing of delayed states and multithreading achivement unlocked

--- src/history.rs
+++ src/history.rs
@@ -1,16 +1,14 @@
-use std::collections::HashMap;
-
 use chrono::{DateTime, Utc};
-use serde::Serialize;
 use serde_json::Value;
+use std::collections::HashMap;
 
 use crate::{merge, parser};
 
-#[derive(Debug, Serialize)]
+#[derive(Debug)]
 pub struct History {
     pub initial: Option<Value>,
     pub updates: Vec<parser::Update>,
-    pub delay_states: HashMap<String, Value>,
+    pub delay_states: HashMap<i64, AsyncState>,
     pub realtime: Option<Realtime>,
 }
 
@@ -45,15 +43,112 @@ impl History {
 
         if let Some(ref mut realtime) = self.realtime {
             for update in updates {
-                realtime.timestamp = Some(update.timestamp);
+                // realtime.timestamp = Some(update.timestamp);
+                realtime.timestamp = Some(Utc::now());
                 merge::merge(&mut realtime.state, &update.state);
             }
         }
     }
+
+    pub fn get_all_delayed(&mut self) -> HashMap<i64, Value> {
+        let mut map = HashMap::new();
+
+        for (k, _) in self.delay_states.clone() {
+            if let Some(val) = self.get_delayed(&k) {
+                map.insert(k.to_owned(), val);
+            }
+        }
+
+        map
+    }
+
+    pub fn get_delayed(&mut self, delay: &i64) -> Option<Value> {
+        if let Some(existing) = self.delay_states.get_mut(&delay) {
+            let delayed_timestamp = chrono::Utc::now().timestamp() - delay;
+
+            if existing.next_timestamp.timestamp() >= delayed_timestamp {
+                let mut latest_update_index: usize = 0;
+
+                for (pos, update) in self.updates.iter().enumerate() {
+                    if update.timestamp.timestamp() > existing.current_timestamp.timestamp() {
+                        continue;
+                    };
+
+                    if update.timestamp.timestamp() > delayed_timestamp {
+                        continue;
+                    };
+
+                    latest_update_index = pos;
+
+                    merge::merge(&mut existing.state, &update.state);
+                }
+
+                let current = self.updates.get(latest_update_index).unwrap();
+                let next = self.updates.get(latest_update_index + 1).unwrap();
+
+                existing.current_timestamp = current.timestamp;
+                existing.next_timestamp = next.timestamp;
+            }
+
+            return Some(existing.state.clone());
+        };
+
+        if let Some(initial_state) = &self.initial {
+            let mut base = initial_state.clone();
+
+            let delayed_timestamp = chrono::Utc::now().timestamp() - delay;
+
+            let mut latest_update_index: usize = 0;
+
+            for (pos, update) in self.updates.iter().enumerate() {
+                if update.timestamp.timestamp() <= delayed_timestamp {
+                    continue;
+                };
+
+                if update.timestamp.timestamp() > delayed_timestamp {
+                    continue;
+                };
+
+                latest_update_index = pos;
+
+                merge::merge(&mut base, &update.state);
+            }
+
+            let current = self.updates.get(latest_update_index).unwrap();
+            let next = self.updates.get(latest_update_index + 1).unwrap();
+
+            let async_state = AsyncState {
+                state: base,
+                current_timestamp: current.timestamp,
+                next_timestamp: next.timestamp,
+            };
+
+            self.delay_states
+                .insert(delay.to_owned(), async_state.clone());
+            return Some(async_state.state);
+        }
+
+        None
+    }
+
+    pub fn get_realtime(&self) -> Option<Value> {
+        if let Some(realtime) = &self.realtime {
+            return Some(realtime.state.clone());
+        }
+
+        None
+    }
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug)]
 pub struct Realtime {
     timestamp: Option<DateTime<Utc>>,
     state: Value,
+}
+
+#[derive(Debug, Clone)]
+pub struct AsyncState {
+    pub state: Value,
+    pub current_timestamp: DateTime<Utc>,
+    pub next_timestamp: DateTime<Utc>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,116 @@
+use std::{
+    collections::HashMap,
+    sync::{mpsc, Arc, Mutex},
+    thread::{self},
+    time::Duration,
+};
+
+use serde_json::Value;
+
+use crate::parser::ParsedMessage;
+
 mod file_loader;
 mod history;
 mod merge;
 mod parser;
 
 fn main() {
-    println!("loading file...");
+    let (history_tx, history_rx) = mpsc::channel::<ParsedMessage>();
+    let (realtime_tx, realtime_rx) = mpsc::channel::<Option<Value>>();
+    let (delay_request_tx, delay_request_rx) = mpsc::channel::<i64>();
+    let (delay_response_tx, delay_response_rx) = mpsc::channel::<HashMap<i64, Value>>();
 
-    let Ok(messages) = file_loader::load_file() else {
-        println!("failed reading file...");
-        return;
-    };
+    let history = Arc::new(Mutex::new(history::History::new()));
 
-    println!("parsing");
+    let history_recive = Arc::clone(&history);
+    let history_delay = Arc::clone(&history);
+    let history_delay_request = Arc::clone(&history);
 
-    let mut history: history::History = history::History::new();
+    let history_recive_handle = thread::spawn(move || {
+        for mut recived_message in history_rx {
+            let mut hist = history_recive.lock().unwrap();
 
-    for message in messages {
-        let mut parsed = parser::parse_message(message);
+            println!("history: got message");
 
-        match parsed {
-            parser::ParsedMessage::Empty => (),
-            parser::ParsedMessage::Replay(state) => history.set_intitial(state),
-            parser::ParsedMessage::Updates(ref mut updates) => {
-                history.add_updates(updates);
-            }
+            match recived_message {
+                parser::ParsedMessage::Empty => (),
+                parser::ParsedMessage::Replay(state) => hist.set_intitial(state),
+                parser::ParsedMessage::Updates(ref mut updates) => {
+                    hist.add_updates(updates);
+                }
+            };
+
+            let _ = realtime_tx.send(hist.get_realtime());
+        }
+    });
+
+    let history_delay_handle = thread::spawn(move || loop {
+        println!("history: delayed: getting new data");
+        let mut hist = history_delay.lock().unwrap();
+        let updated_states = hist.get_all_delayed();
+        let _ = delay_response_tx.send(updated_states);
+
+        thread::sleep(Duration::from_millis(100));
+    });
+
+    let history_delay_request_handle = thread::spawn(move || {
+        for request in delay_request_rx {
+            let mut hist = history_delay_request.lock().unwrap();
+            println!("history: delayed: got request for {}", request);
+            hist.get_delayed(&request);
+        }
+    });
+
+    let file_handle = thread::spawn(move || {
+        println!("file: reading");
+
+        let Ok(messages) = file_loader::load_file() else {
+            println!("failed reading file...");
+            return;
         };
-    }
 
-    println!("realtime: {:?}", history.realtime);
-    println!("updates: {}", history.updates.len());
+        println!("file: parsing");
 
-    println!("done");
+        for message in messages {
+            let parsed = parser::parse_message(message);
+            let _ = history_tx.send(parsed);
+            thread::sleep(Duration::from_millis(100));
+        }
+
+        println!("file: done");
+    });
+
+    let realtime_handle =
+        thread::spawn(|| {
+            println!("realtime: listening");
+
+            for _ in realtime_rx {
+                println!("realtime: got state");
+            }
+
+            println!("realtime: done");
+        });
+
+    let delayed_handle = thread::spawn(move || {
+        println!("delayed: listening");
+
+        let delay: i64 = 10;
+
+        println!("delayed: requesting");
+        let _ = delay_request_tx.send(delay);
+
+        for response in delay_response_rx {
+            println!("delayed: got response");
+            if let Some(state) = response.get(&delay) {
+                println!("delayed: got state for delay: {}: {}", delay, state);
+            }
+        }
+    });
+
+    file_handle.join().unwrap();
+    history_recive_handle.join().unwrap();
+    history_delay_handle.join().unwrap();
+    realtime_handle.join().unwrap();
+    delayed_handle.join().unwrap();
+    history_delay_request_handle.join().unwrap();
 }
